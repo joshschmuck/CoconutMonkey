4e5502801736d363aab3885f0ae2667caa0624e9
Joshua Schmuck <josh.schmuck@gmail.com>
2017-01-11 20:33:03 -0800

    Initial SetUp
    Set up Generic Repository and extracted interface. Also created model
    for posting a movie.

--- src/CoconutMonkey/Data/ApplicationDbContext.cs
+++ src/CoconutMonkey/Data/ApplicationDbContext.cs
@@ -15,6 +15,8 @@ namespace CoconutMonkey.Data
         {
         }
 
+        public DbSet<Movie> Movies { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
---
+++ src/CoconutMonkey/Repositories/GenericRepository.cs
@@ -0,0 +1,57 @@
+﻿using CoconutMonkey.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoconutMonkey.Interfaces
+{
+    public class GenericRepository
+    {
+        private ApplicationDbContext _db;
+
+        public IQueryable<T> Query<T>() where T : class
+        {
+            return _db.Set<T>().AsQueryable();
+        }
+
+        public void Add<T>(T entityToCreate) where T : class
+        {
+            _db.Set<T>().Add(entityToCreate);
+            _db.SaveChanges();
+        }
+
+        public void Update<T>(T entityToUpdate) where T : class
+        {
+            _db.Set<T>().Update(entityToUpdate);
+            this.SaveChanges();
+        }
+
+        public void Delete<T>(T entityToDelete) where T : class
+        {
+            _db.Set<T>().Remove(entityToDelete);
+            this.SaveChanges();
+        }
+
+        public IQueryable<T> SqlQuery<T>(string sql, params object[] parameters) where T : class
+        {
+            return _db.Set<T>().FromSql(sql, parameters);
+        }
+
+        public void SaveChanges()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+
+        public GenericRepository(ApplicationDbContext db)
+        {
+            this._db = db;
+        }
+    }
+}
